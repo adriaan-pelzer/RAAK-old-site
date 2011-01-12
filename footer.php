89c162058d6fb29883dc193e20618c2de3b61af7
Adriaan Pelzer <adriaan@wewillraakyou.com>
2011-01-12 10:36:24 +0000

    Add RAAK.it to footer, and add nofollows

--- footer.php
+++ footer.php
@@ -1,13 +1,15 @@
 
 	<div id="footer">
         <span id="footer_copyright">&copy; RAAK <?php echo strftime ("%Y"); ?></span>
-        <span class="footer_link"><a href="http://www.youtube.com/user/wewillRAAKyou">RAAK on Youtube</a></span>
+        <span class="footer_link"><a title="RAAK.it URL shortener" href="http://raak.it">RAAK.it</a></span>
         <span class="footer_seperator">|</span>
-        <span class="footer_link"><a href="http://www.twitter.com/RAAKonteurs">RAAK on Twitter</a></span>
+        <span class="footer_link"><a rel="nofollow" href="http://www.youtube.com/user/wewillRAAKyou">RAAK on Youtube</a></span>
         <span class="footer_seperator">|</span>
-        <span class="footer_link"><a href="http://www.linkedin.com/companies/raak">RAAK on LinkedIn</a></span>
+        <span class="footer_link"><a rel="nofollow" href="http://www.twitter.com/RAAKonteurs">RAAK on Twitter</a></span>
         <span class="footer_seperator">|</span>
-        <span class="footer_link"><a href="http://www.facebook.com/pages/RAAK/167105176615">RAAK on Facebook</a></span>
+        <span class="footer_link"><a rel="nofollow" href="http://www.linkedin.com/companies/raak">RAAK on LinkedIn</a></span>
+        <span class="footer_seperator">|</span>
+        <span class="footer_link"><a rel="nofollow" href="http://www.facebook.com/pages/RAAK/167105176615">RAAK on Facebook</a></span>
         <!--span class="footer_seperator">|</span>
         <span class="footer_link"><a href="http://www.flickr.com">RAAK on Flickr</a></span-->
 	</div><!-- #footer -->
