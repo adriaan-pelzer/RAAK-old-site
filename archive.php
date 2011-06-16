92e62b6a10d3d07d0c3372f1ae3eced83537ea35
Adriaan Pelzer <adriaan.pelzer@gmail.com>
2011-06-16 18:19:23 +0100

    Add class to fix archive

--- archive.php
+++ archive.php
@@ -60,7 +60,7 @@ if (isset ($_GET['authorid'])) {
             <div id="whitebox_primary">
                 <div id="whitebox_primary_top">
                 </div><!-- #whitebox_primary_top -->
-                <div id="whitebox_primary_body">
+                <div id="whitebox_primary_body" class="whitebox_primary_body_archive">
 <?php
 $i = 0;
 
